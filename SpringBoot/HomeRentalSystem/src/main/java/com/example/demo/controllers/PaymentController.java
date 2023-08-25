4ff6ff2bee79c76be8124c303a8afb39976f2479
unknown <shrikantrane1999@gmail.com>
2023-08-25 14:42:00 +0530

    api

--- SpringBoot/HomeRentalSystem/src/main/java/com/example/demo/controllers/PaymentController.java
+++ SpringBoot/HomeRentalSystem/src/main/java/com/example/demo/controllers/PaymentController.java
@@ -1,24 +1,62 @@
 package com.example.demo.controllers;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entities.Area;
+import com.example.demo.entities.Login;
+import com.example.demo.entities.Owner;
+import com.example.demo.entities.Payment;
+import com.example.demo.entities.PaymentReg;
+import com.example.demo.entities.Role;
+import com.example.demo.entities.Subscription;
+import com.example.demo.entities.Tenant;
+import com.example.demo.entities.TenantReg;
+import com.example.demo.services.LoginService;
+import com.example.demo.services.OwnerService;
 import com.example.demo.services.PaymentService;
+import com.example.demo.services.SubscriptionService;
 
 @RestController
 public class PaymentController {
 	
 	@Autowired
 	PaymentService pservice;
-	
+	@Autowired
+	LoginService lservice;
+	@Autowired
+	SubscriptionService sservice;
+	@Autowired
+	OwnerService oservice;
 	 @GetMapping("/updateloginstatus")
 	 public ResponseEntity<String> updateLoginStatusBasedOnPayments() 
 	 {
 	     pservice.updateLoginStatusBasedOnPayments();
 	     return ResponseEntity.ok("Login statuses updated based on payment subscription.");
 	 }
+	 
+	    @PostMapping("/regpayment")
+		public Payment regPayment(@RequestBody PaymentReg pr)
+		{
+			
+			Login l=lservice.getLogin(pr.getLogin_id());
+			Subscription subscription=sservice.getSub(pr.getSubscription_id());
+			
+			
+			Payment payment=new Payment(new Date(0), pr.getAmount(), null, l, subscription);
+			Payment saved=pservice.save(payment);
+			//Owner o=pay
+			
+			
+			return saved;
+		}
 	
 
 }
--- SpringBoot/HomeRentalSystem/src/main/java/com/example/demo/entities/PaymentReg.java
+++ SpringBoot/HomeRentalSystem/src/main/java/com/example/demo/entities/PaymentReg.java
@@ -5,6 +5,36 @@ public class PaymentReg {
 	int no_of_requests,no_of_properties;
 	float amount;
 	int login_id,subscription_id;
-	
+	public int getNo_of_requests() {
+		return no_of_requests;
+	}
+	public void setNo_of_requests(int no_of_requests) {
+		this.no_of_requests = no_of_requests;
+	}
+	public int getNo_of_properties() {
+		return no_of_properties;
+	}
+	public void setNo_of_properties(int no_of_properties) {
+		this.no_of_properties = no_of_properties;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public int getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(int login_id) {
+		this.login_id = login_id;
+	}
+	public int getSubscription_id() {
+		return subscription_id;
+	}
+	public void setSubscription_id(int subscription_id) {
+		this.subscription_id = subscription_id;
+	}
+	 
 
 }
